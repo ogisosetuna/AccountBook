d6cbc1cb1f60111fef44fdcc588bf90826d075d7
ogisosetuna <852408680@qq.com>
2021-02-27 21:55:01 +0900

    Add function (count and spend) debug

--- app/src/main/java/com/example/AccountBook/TwoFragment.java
+++ app/src/main/java/com/example/AccountBook/TwoFragment.java
@@ -1,18 +1,74 @@
 package com.example.AccountBook;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+
 
 import androidx.fragment.app.Fragment;
 
 public class TwoFragment extends Fragment {
+    private PieView mPieView;
+    private TextView tvAdd;
+    private TextView tvReduce;
+
     @Override
-
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstancesState){
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container,false);
+        View v = inflater.inflate(R.layout.fragment_two, container,false);
+        mPieView = v.findViewById(R.id.pieView);
+        initPieView();
+        initAddAndReduce();
+        return v;
+    }
+
+    private void initPieView() {
+        mPieView.setColors(createColors());
+        mPieView.setData(createData());
+    }
+
+    private ArrayList<PieEntry> createData() {
+        ArrayList<PieEntry> pieLists = new ArrayList<>();
+        pieLists.add(new PieEntry(20.00F, "服装"));
+        pieLists.add(new PieEntry(20.00F, "数码产品"));
+        pieLists.add(new PieEntry(20.00F, "保健品"));
+        pieLists.add(new PieEntry(20.00F, "户外运动用品"));
+        pieLists.add(new PieEntry(20.00F, "其他"));
+        return pieLists;
+    }
+
+    private ArrayList<Integer> createColors() {
+        ArrayList<Integer> colorLists = new ArrayList<>();
+        colorLists.add(Color.parseColor("#EBBF03"));
+        colorLists.add(Color.parseColor("#ff4d4d"));
+        colorLists.add(Color.parseColor("#8d5ff5"));
+        colorLists.add(Color.parseColor("#41D230"));
+        colorLists.add(Color.parseColor("#4FAAFF"));
+        return colorLists;
+    }
+
+    private void initAddAndReduce() {
+        tvAdd = tvAdd.findViewById(R.id.tv_add);
+        tvReduce = tvReduce.findViewById(R.id.tv_reduce);
+        tvAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup.LayoutParams layoutParams = mPieView.getLayoutParams();
+                layoutParams.height = mPieView.getMeasuredHeight() + 10;
+                mPieView.setLayoutParams(layoutParams);
+            }
+        });
+        tvReduce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup.LayoutParams layoutParams = mPieView.getLayoutParams();
+                layoutParams.height = mPieView.getMeasuredHeight() - 10;
+                mPieView.setLayoutParams(layoutParams);
+            }
+        });
     }
 }
